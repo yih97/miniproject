b3680e92e6c65e5cd22fe6180228c8020de28a8e
KoByoungWook <141305275+KoByoungWook@users.noreply.github.com>
2023-08-23 15:03:17 +0900

    community
    super계정 생성
    admin 생성
    community 페이지 생성

--- community/models.py
+++ community/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Community(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField(blank=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- team_project/urls.py
+++ team_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("single_page/", include("single_page.urls")),
+    path("", include("single_page.urls")),
     path("coffee/", include("coffee.urls")),
+    path("community/", include("community.urls")),
 ]
