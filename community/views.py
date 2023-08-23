f51975fcf4ff6cf6ffd283b83f43c0ba81ce7811
BangBnag22 <122610370+yih97@users.noreply.github.com>
2023-08-23 15:24:44 +0900

    살려주세요
    복구해주세요..

--- community/views.py
+++ community/views.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Community
 from django.views.generic import ListView, DetailView
@@ -6,3 +7,15 @@ from django.views.generic import ListView, DetailView
 def community(request):
     return render(request, "community/community.html")
 
+=======
+from django.shortcuts import render
+from .models import Post
+
+def community_page(request):
+    posts = Post.objects.all()
+    return render(
+        request,
+        "community/community.html",
+        {"posts": posts}
+    )
+>>>>>>> Stashed changes
