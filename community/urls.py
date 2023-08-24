bf0c1fa102828d0aa1c7ba473c41d6e28aad30e5
KoByoungWook <141305275+KoByoungWook@users.noreply.github.com>
2023-08-24 15:14:59 +0900

    병욱/작업5
    게시글 작성
    게시글 수정

--- community/urls.py
+++ community/urls.py
@@ -5,10 +5,11 @@ from django.contrib.auth import views as auth_views
 app_name = "community"
 
 urlpatterns = [
-    path("", views.community),
+    path("", views.community, name = "community"),
     path('login/', auth_views.LoginView.as_view(template_name='community/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('<int:community_id>/', views.detail, name='detail'),
-    path('create/', views.community_create, name='community_create'),
+    path('<int:pk>/', views.community_detail, name='community_detail'),
+    path('new/', views.new, name='new'),
+    path('update/<int:pk>/', views.update, name='update'),
 ]
