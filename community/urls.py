f82465d0d39c2e74265541640c12401ca555351c
YuJeongMinn <polar522@naver.com>
2023-08-24 15:16:08 +0900

    정민
    커뮤니티 수정

--- community/urls.py
+++ community/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='community/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup, name='signup'),
-    path('<int:community_id>/', views.detail, name='detail'),
-    path('create/', views.community_create, name='community_create'),
+    path('<int:pk>/', views.community_detail, name='community_detail'),
+    path('new/', views.new, name='new'),
+    path('modify/<int:community_id>/', views.modify, name='modify'),
+    
 ]
