9cf88f425686b46ea3c5ab54d740addba11cf829
YuJeongMinn <polar522@naver.com>
2023-08-24 17:48:29 +0900

    community앱에서 login기능구현
    community앱에서 login기능구현

--- community/models.py
+++ community/models.py
@@ -14,6 +14,20 @@ class Community(models.Model):
 
     class Meta:
         verbose_name_plural = "Communities"
+
+
+
+class Coffee(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    brand = models.CharField(max_length=10)     # 브랜드
+    coffee = models.CharField(max_length=20)    # 커피 종류
+    coffee_name = models.CharField(max_length=20)
+    price = models.IntegerField()     # 가격
+    caffeine = models.IntegerField(null=True)      # 카페인
+    sugar = models.IntegerField(null=True)     # 당
+    kcal = models.IntegerField(null=True)       # 칼로리
+
+
         
 
 
