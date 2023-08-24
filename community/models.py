b5339bc06847217fc505ccdaedb0dd423cf96104
YuJeongMinn <polar522@naver.com>
2023-08-24 09:50:09 +0900

    로그인, 회원가입
    로그인, 회원가입

--- community/models.py
+++ community/models.py
@@ -1,18 +1,16 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 class Community(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField(blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
 
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+
